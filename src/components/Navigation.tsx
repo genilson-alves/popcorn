cdfdbffc69043ff0130706bfd7d922787e676c68
genilson-alves <genilson.alves9555@gmail.com>
2024-01-10 11:13:50 -0300

    Mobile navigation bar in development

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { COLORS } from "../Styled";
 import { Link } from "react-router-dom";
 const logo = require("../assets/logo.png");
+const hamburger = require("../assets/hamburger.png");
+const close = require("../assets/close.png");
 
-const NavigationContainer = styled.div`
+const NavigationContainer = styled.div<{ showMenu: boolean }>`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   padding: 10px;
   background-color: ${COLORS.NAVIGATION_FOOTER_BACKGROUND_COLOR};
   color: ${COLORS.FOOTER_COLOR};
-  @media (min-width: 320px) and (max-width: 768px) {
+  @media (max-width: 768px) {
+    flex-direction: ${(props) => (props.showMenu ? "column" : "row")};
+    padding: 10px 20px;
+    align-items: center;
+  }
+`;
+
+const Logo = styled.div`
+  img {
+    height: 50px;
+  }
+`;
+
+const NavigationLinks = styled.div<{ showMenu: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  @media (max-width: 768px) {
     flex-direction: column;
+    align-items: center;
+    width: 100%;
+    display: ${(props) => (props.showMenu ? "flex" : "none")};
+  }
+`;
+
+const LogoHamburger = styled.div`
+  @media (max-width: 768px) {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
 `;
 
@@ -26,35 +58,37 @@ const NavigationLink = styled(Link)`
   }
 `;
 
-const Logo = styled.div`
-  img {
-    height: 50px;
+const HamburgerButton = styled.div<{ showMenu: boolean }>`
+  display: none;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    display: ${(props) => (props.showMenu ? "none" : "block")};
+    transition: 0.3s;
+    img {
+      width: 40px;
+    }
   }
 `;
 
-const NavigationLinks = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 20px;
-`;
-
-const DropdownContent = styled.div`
+const CloseButton = styled.div<{ showMenu: boolean }>`
   display: none;
-  padding: 5px;
-  position: absolute;
-  background-color: ${COLORS.PAGE_WHITE};
-  min-width: 160px;
-  z-index: 1;
+  cursor: pointer;
+  @media (max-width: 768px) {
+    display: ${(props) => (props.showMenu ? "block" : "none")};
+    transition: 0.3s;
+    img {
+      width: 40px;
+    }
+  }
 `;
 
 const Dropdown = styled.div`
   position: relative;
-  &:hover {
-    ${DropdownContent} {
-      display: block;
-      border-radius: 10px;
-    }
+  margin-right: 20px;
+
+  @media (max-width: 768px) {
+    margin: 10px 0;
   }
 `;
 
@@ -70,6 +104,26 @@ const DropdownButton = styled.button`
   }
 `;
 
+const DropdownContent = styled.div`
+  display: none;
+  padding: 5px;
+  position: absolute;
+  background-color: ${COLORS.PAGE_WHITE};
+  min-width: 160px;
+  z-index: 1;
+
+  ${Dropdown}:hover & {
+    display: block;
+    border-radius: 10px;
+  }
+
+  @media (max-width: 768px) {
+    display: block;
+    position: static;
+    width: 100%;
+  }
+`;
+
 const DropdownOption = styled(Link)`
   color: ${COLORS.NAVIGATION_FOOTER_BACKGROUND_COLOR};
   background-color: ${COLORS.PAGE_WHITE};
@@ -77,6 +131,7 @@ const DropdownOption = styled(Link)`
   padding: 12px;
   text-decoration: none;
   display: block;
+
   &:hover {
     background-color: #fff;
     text-decoration: underline;
@@ -86,6 +141,10 @@ const DropdownOption = styled(Link)`
 const User = styled.div`
   display: flex;
   align-items: center;
+  @media (max-width: 768px) {
+    display: none;
+    margin: 10px 0;
+  }
 `;
 
 const Sign = styled(Link)`
@@ -96,20 +155,35 @@ const Sign = styled(Link)`
   padding: 15px;
   color: white;
   background-color: black;
+
   &:hover {
     text-decoration: underline;
   }
 `;
 
 const Navigation: React.FC = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <NavigationContainer>
-      <Logo>
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-      </Logo>
-      <NavigationLinks>
+    <NavigationContainer showMenu={showMenu}>
+      <LogoHamburger>
+        <Logo>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </Logo>
+        <HamburgerButton showMenu={showMenu} onClick={toggleMenu}>
+          <img src={hamburger} alt="hamburger" />
+        </HamburgerButton>
+        <CloseButton showMenu={showMenu} onClick={toggleMenu}>
+          <img src={close} alt="close" />
+        </CloseButton>
+      </LogoHamburger>
+      <NavigationLinks showMenu={showMenu}>
         <NavigationLink to="/search">Search</NavigationLink>
         <Dropdown>
           <DropdownButton>Movies</DropdownButton>
